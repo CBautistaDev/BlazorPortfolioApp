8939c938e91fa086fc85e7c9722f20bd5767474a
Carlos <Carlos.Bautista@experian.com>
2021-10-01 08:55:36 -0700

    Add project files.

---
+++ Pages/Projects.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioApp.Pages
+{
+    public partial class Projects
+    {
+    }
+}
